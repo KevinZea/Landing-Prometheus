d31e99e2e51c22d3e02e59d0700dd5a30446b047
Diego Andres Hernandez Moreno <diego.hernandez@jala.university>
2025-02-19 20:50:22 -0500

    update(app): delete unused route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import {ChakraProvider} from "@chakra-ui/react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout.tsx";
 import Landing from "./components/Landing.tsx";
-import SearchResults from "./components/SearchResults.tsx";
 import '@splidejs/splide/dist/css/splide.min.css';
 import theme from "./theme.tsx";
 
 function App() {
-    const hotelId: string = 'cm7agdu2t0006ne0w5rh1jc0s';
 
   return (
     <ChakraProvider theme={theme}>
@@ -16,7 +14,6 @@ function App() {
           <Routes>
               <Route path="/" element={<Layout />}>
                   <Route index element={<Landing />}/>
-                  <Route path="search" element={<SearchResults hotelId={hotelId}/>} />
               </Route>
           </Routes>
       </BrowserRouter>
